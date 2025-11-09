ff9b2706e48a7fe353b1edfe4101a62ec569855c
Md Younus Ali <younusidb@gmail.com>
2025-11-09 18:39:15 +0600

    Project Create

---
+++ School_Management_System/Models/Departments.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School_Management_System.Models
+{
+    public class Departments
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public ICollection<Students> Students { get; set; }
+    }
+}
